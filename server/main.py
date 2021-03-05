d3bfdd7221281fc0739df622e8629c98a0f51f4e
Ilyasse Fakhreddine <ilyasse.fakhreddine@coopersoftware.co.uk>
2021-03-05 19:30:03 +0000

    Renamed folders

--- server/main.py
+++ server/main.py
@@ -4,7 +4,7 @@ from werkzeug.routing import BaseConverter
 
 
 app = Flask(__name__, static_url_path="",
-            static_folder="../wizard-of-oz-interface/dist/wizard-of-oz-interface")
+            static_folder="../client/dist/wizard-of-oz-interface")
 
 class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
@@ -16,11 +16,11 @@ app.url_map.converters['regex'] = RegexConverter
 
 @app.route("/")
 def angular():
-    return send_from_directory("../wizard-of-oz-interface/dist/wizard-of-oz-interface", "index.html")
+    return send_from_directory("../client/dist/wizard-of-oz-interface", "index.html")
 
 @app.route("/<regex('\w\.(js|css)'):path>")
 def angular_src(path):
-    return send_from_directory("../wizard-of-oz-interface/dist/wizard-of-oz-interface", path)
+    return send_from_directory("../client/dist/wizard-of-oz-interface", path)
 
 @app.route("/hello")
 def hello():
